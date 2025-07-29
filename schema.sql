28e19f2d1b4619840d3ad7a79a6ed18de1ee75ac
Srinitya75 <Kvn.srinitya@gmail.com>
2025-07-29 16:10:22 +0530

    schema.sql

--- schema.sql
+++ schema.sql
@@ -5,9 +5,3 @@ CREATE TABLE IF NOT EXISTS users (
     password TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS products (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    description TEXT,
-    price REAL
-);
